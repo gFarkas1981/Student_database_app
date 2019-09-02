89bded779de1e09fa3b57ee03a88ca7ef38ca23d
gFarkas1981 <info@gfarkas.com>
2019-09-02 10:20:25 +0200

    First Commit

---
+++ src/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse;
+
+    // Constructor: prompt user to enter student's name and year
+
+    // Generate an ID
+
+    // Enroll in courses
+
+    // View balance
+
+    // Pay tuition
+
+    // Show status
+
+
+}
---
+++ src/StudentDataBaseApp.java
@@ -0,0 +1,11 @@
+public class StudentDataBaseApp {
+
+    public static void main(String[] args) {
+
+        // Ask how many new users we want to add
+
+        // Create n number of new students
+
+    }
+
+}

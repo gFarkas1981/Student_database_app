8e0e913932163bc60d5d81972754339e6beda885
gFarkas1981 <info@gfarkas.com>
2019-09-02 11:42:16 +0200

    Implementing the paTuition method

--- src/Student.java
+++ src/Student.java
@@ -45,7 +45,7 @@ public class Student {
 
         // Get inside a loop, user hits q
         while (true) {
-            System.out.print("Enter course to enroll (Q to quit): ");
+            System.out.print("Enter course to enroll (Q to quit):");
 
             Scanner in = new Scanner(System.in);
             String course = in.nextLine();
@@ -70,8 +70,24 @@ public class Student {
 
 
     // View balance
+    public void viewBalance() {
+
+        System.out.println("Your balance is: $" + tuitionBalance);
+
+    }
 
     // Pay tuition
+    public void payTuition() {
+
+        viewBalance();
+        System.out.println("Enter your payment: $");
+        Scanner in = new Scanner(System.in);
+        int payment = in.nextInt();
+        tuitionBalance = tuitionBalance - payment;
+        System.out.println("Thank you for your payment of $" + payment);
+        viewBalance();
+
+    }
 
     // Show status
 
--- src/StudentDataBaseApp.java
+++ src/StudentDataBaseApp.java
@@ -4,6 +4,7 @@ public class StudentDataBaseApp {
 
         Student stu1 = new Student();
         stu1.enroll();
+        stu1.payTuition();
 
         // Ask how many new users we want to add
 
